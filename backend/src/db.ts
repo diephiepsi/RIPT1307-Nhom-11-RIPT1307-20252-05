7e23752c5a03b11314d5cfa2238d07cddfab3403
htuyn <tuyenvh.ptit@gmail.com>
2026-06-08 15:50:47 +0700

    add

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,17 +1,17 @@
+// src/db.ts
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
 const connectionString = process.env.DATABASE_URL;
+if (!connectionString) throw new Error("DATABASE_URL is required");
 
-if (!connectionString) {
-  throw new Error("DATABASE_URL is required");
-}
-
-const adapter = new PrismaPg({
+const pool = new Pool({
   connectionString,
+  ssl: { rejectUnauthorized: false }, // ← THÊM DÒNG NÀY
 });
 
-export const prisma = new PrismaClient({
-  adapter,
-});
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({ adapter });
